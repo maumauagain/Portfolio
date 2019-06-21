a346499c9b17ddd5e800aa1655bf53618ee30066
Amauri Martins Junior <amauri@forlogic.com>
2019-06-21 11:29:44 -0300

    Updates

---
+++ precache-manifest.15406538f96556100b671144fba9ccce.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "211707cf4a8267fef93ae6dea37a5be5",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "c70d4e572e9fdb77fa6f",
+    "url": "/Portfolio/static/css/main.a1c7f0c7.chunk.css"
+  },
+  {
+    "revision": "917baa027c1993b63d96",
+    "url": "/Portfolio/static/js/2.16989715.chunk.js"
+  },
+  {
+    "revision": "c70d4e572e9fdb77fa6f",
+    "url": "/Portfolio/static/js/main.66a83bc6.chunk.js"
+  },
+  {
+    "revision": "6b64d540f1308ee928ad",
+    "url": "/Portfolio/static/js/runtime~main.132fbc8c.js"
+  },
+  {
+    "revision": "9c50939dfc551e71e4cd22b6b31cb0a5",
+    "url": "/Portfolio/static/media/bg-contact.9c50939d.jpg"
+  },
+  {
+    "revision": "76d344f08412543c74ccd1fb2493d607",
+    "url": "/Portfolio/static/media/icon-github.76d344f0.svg"
+  },
+  {
+    "revision": "df721c2187edc80e4b51abf012cfefc5",
+    "url": "/Portfolio/static/media/icon-linkedin.df721c21.png"
+  },
+  {
+    "revision": "9badd752150ac472735d97067b858f27",
+    "url": "/Portfolio/static/media/portfolio-1.9badd752.PNG"
+  },
+  {
+    "revision": "4ca1a0a1f565f995faee6af2e5c51932",
+    "url": "/Portfolio/static/media/portfolio-2.4ca1a0a1.PNG"
+  },
+  {
+    "revision": "13389e180f2a22be4c4172b215d5513d",
+    "url": "/Portfolio/static/media/post-1.13389e18.jpg"
+  },
+  {
+    "revision": "13d61ffc773534984fe582f5db1eddee",
+    "url": "/Portfolio/static/media/post-2.13d61ffc.jpg"
+  },
+  {
+    "revision": "8a89ab55539aaf58fbb4f39fb76f8f39",
+    "url": "/Portfolio/static/media/post-3.8a89ab55.jpg"
+  },
+  {
+    "revision": "4fdac71471d554917cad7197247b6b8f",
+    "url": "/Portfolio/static/media/profile.4fdac714.jpg"
+  }
+]);
