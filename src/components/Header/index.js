1521327d725e1f92d4edff5f0a4cb706630f55bc
Amauri Martins Junior <amauri@forlogic.com>
2019-06-21 14:45:06 -0300

    Adding responsivity for the portfolio

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import "./styles.css";
 
-const Header = () => 
-<header id="main-header">
-    <ul className="menu">
-        <a href="#about"><li>Inicio</li></a>
-        <a href="#Skills"><li>Habilidades</li></a>
-        <a href="#Blog"><li>Blog</li></a>
-        <a href="#Portfolio"><li>Portfolio</li></a>
-        <a href="#bio"><li>Detalhes</li></a>
-        <a href="#Contact"><li>Contato</li></a>
-    </ul>
-</header>;
+const Header = () =>
+    <header id="main-header">
+        <ul className="menu">
+            <a href="#about"><li>Inicio</li></a>
+            <a href="#Skills"><li>Habilidades</li></a>
+            <a href="#Blog"><li>Blog</li></a>
+            <a href="#Portfolio"><li>Portfolio</li></a>
+            <a href="#bio"><li>Detalhes</li></a>
+            <a href="#Contact"><li>Contato</li></a>
+        </ul>
+        <p className="hidden">Amauri Martins Junior</p>
+    </header>;
 
 export default Header;
