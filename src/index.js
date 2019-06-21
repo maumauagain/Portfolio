ef7416b38846d1dfe406a2e545602b14a3de3c1f
Amauri Martins Junior <amauri@forlogic.com>
2019-06-21 14:55:26 -0300

    Sending the change for the title

--- src/index.js
+++ src/index.js
@@ -3,5 +3,7 @@ import ReactDOM from 'react-dom';
 import Main from './pages/Main'
 import './index.css';
 
+document.title = "Amauri Martins";
+
 ReactDOM.render(<Main />, document.getElementById('root'));
 
